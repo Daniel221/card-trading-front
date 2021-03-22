174c4e6679c347f169dc5f82c4ad92d3e3165f2d
Daniel Hernandez <daniel66670@hotmail.com>
2021-03-22 15:23:07 -0600

    added register-form, httpClient & proxy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { FormComponent } from './form/form.component';
 import { CardComponent } from './card/card.component';
+import { RegisterFormComponent } from './register-form/register-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FormComponent,
-    CardComponent
+    CardComponent,
+    RegisterFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/register-form/register-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-register-form',
+  templateUrl: './register-form.component.html',
+  styleUrls: ['./register-form.component.less']
+})
+export class RegisterFormComponent {
+  @Input() value: String;
+  @Input() type: 'text' | 'email' | 'password' = 'text';
+  @Input() label: string;
+  @Output() valueChange = new EventEmitter<string>();
+
+  onAdd(e: Event) {
+    this.valueChange.emit((<HTMLInputElement>e.target).value);
+  }
+}
