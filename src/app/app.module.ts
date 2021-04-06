5f816760c746015f57cb9ba75c5cc42f06fb5c63
Alex Maciel <69065904+AlexMacielRamos@users.noreply.github.com>
2021-04-05 21:08:31 -0500

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
@@ -15,6 +16,9 @@ import { CardDetailsComponent } from './card-details/card-details.component';
 import { AppRoutingModule } from './app.routing.module';
 import { UserListComponent } from './user-list/user-list.component';
 import { UserCardComponent } from './user-card/user-card.component';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+
 
 @NgModule({
   declarations: [
@@ -34,9 +38,10 @@ import { UserCardComponent } from './user-card/user-card.component';
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
